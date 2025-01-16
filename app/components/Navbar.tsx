9ea740ec8aa99908999f832e1fab828e66810a73
Pritesh Pagar <priteshpagar9199@gmail.com>
2025-01-16 22:16:12 +0530

    Update Navbar.tsx

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,11 +1,19 @@
-import Link from 'next/link'
+import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Navbar() {
   return (
     <nav className="bg-black bg-opacity-85 text-white p-4">
       <div className="max-w-6xl mx-auto flex justify-center items-center">
+        <Image 
+          src="/path-to-your-image/logo.png" 
+          alt="Logo" 
+          width={40} 
+          height={40} 
+          className="mr-2"
+        />
         <Link href="/" className="text-4xl font-bold tracking-widest">PlantID</Link>
       </div>
     </nav>
-  )
+  );
 }
